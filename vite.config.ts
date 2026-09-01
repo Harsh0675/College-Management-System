5ee9eb7b868832ab038417ea502715c4a7f1f93e
HARSH NAGAR <157012443+Harsh0675@users.noreply.github.com>
2026-09-01 17:57:21 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/College-Management-System/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
